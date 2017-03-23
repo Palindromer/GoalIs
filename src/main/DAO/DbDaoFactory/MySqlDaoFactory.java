a7a4b3c5927e8c2d1b27ca9f855783bf22f7848d
Palindromer <ole-koo@yandex.ua>
2017-03-24 01:00:08 +0200

    Added import to MySqlDaoFactory

--- src/main/DAO/DbDaoFactory/MySqlDaoFactory.java
+++ src/main/DAO/DbDaoFactory/MySqlDaoFactory.java
@@ -3,21 +3,14 @@ package main.DAO.DbDaoFactory;
 
 import main.DAO.IGoalDao;
 import main.DAO.IUserDao;
+import main.DAO.MySqlDao.*;
 
 class MySqlDaoFactory extends DaoFactory {
 
     @Override
-    public IUserDao getUserDao() {
-
-        return new MySqlUserDao();
-
-    }
+    public IUserDao getUserDao() { return new MySqlUserDao(); }
 
     @Override
-    public IGoalDao getGoalDao() {
-
-        return new MySqlGoalDao();
-
-    }
+    public IGoalDao getGoalDao() { return new MySqlGoalDao(); }
 
 }
